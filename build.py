3cfff68ee9ffccfbdcda99f6168887c0f5c97f20
Mauro Delazeri <maurodelazeri@gmail.com>
2019-04-13 20:29:42 -0400

    update

--- build.py
+++ build.py
@@ -1,8 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import tools
-from bincrafters import build_template_default
+from conan.packager import ConanMultiPackager
+
+def main():
+    """
+    Main function.
+    """
+    builder = ConanMultiPackager(username="osechet", channel="testing")
+    builder.add_common_builds()
+    filtered_builds = []
+    for settings, options, env_vars, build_requires in builder.builds:
+        if settings["arch"] != "x86_64":
+            continue
+        if settings["compiler"] == "gcc":
+            settings["compiler.libcxx"] = "libstdc++11"
+
+        filtered_builds.append([settings, options, env_vars, build_requires])
+
+    builder.builds = filtered_builds
+    builder.run()
 
 if __name__ == "__main__":
-    builder.run()
+    main()
