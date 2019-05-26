540e046c2da17896be8c70b84bafc82cdf7b9049
Mauro Delazeri <maurodelazeri@gmail.com>
2019-05-26 19:26:20 -0400

    update

--- conanfile.py
+++ conanfile.py
@@ -49,6 +49,7 @@ class ReadLineConan(ConanFile):
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.libs.append("rdkafka")
         if self.settings.os == "Linux":
             self.cpp_info.libs.append("pthread")
         elif self.settings.os == "Windows":
